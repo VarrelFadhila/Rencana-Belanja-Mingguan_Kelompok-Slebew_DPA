4cfe7e1b137b2945f3c12519f34a1cb8c1aa1780
VarrelFadhila <varrelfadhila@gmail.com>
2025-12-07 16:33:56 +0700

    new file:   module_analisis.py

---
+++ module_analisis.py
@@ -0,0 +1,38 @@
+# module_analisis.py
+
+from module_belanja import belanja
+
+def analisis():
+    print("\n=== Analisis Mingguan ===")
+    if not belanja:
+        print("Tidak ada data.")
+        return
+
+    termahal = belanja[0]
+    termurah = belanja[0]
+    total = 0
+    kategori_total = {}
+
+    for item in belanja:
+        total += item["harga"]
+
+        kat = item["kategori"]
+        kategori_total[kat] = kategori_total.get(kat, 0) + item["harga"]
+
+        if item["harga"] > termahal["harga"]:
+            termahal = item
+        if item["harga"] < termurah["harga"]:
+            termurah = item
+
+    print(f"Barang Termahal: {termahal['nama']} - Rp{termahal['harga']}")
+    print(f"Barang Termurah: {termurah['nama']} - Rp{termurah['harga']}")
+    print("\nPengeluaran Per Kategori:")
+    
+    for kat, t in kategori_total.items():
+        print(f"- {kat}: Rp{t}")
+
+    print("\nTotal Belanja Mingguan :", total)
+    print("Estimasi Bulanan :", total * 4)
+
+    if total > 300000:
+        print("\033[93m⚠️ Anda melebihi 300.000/minggu!\033[0m")
